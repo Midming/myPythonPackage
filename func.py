f55f33002108ec0c84684371b0c279019688eda5
Midming <51870865+Midming@users.noreply.github.com>
2019-06-22 12:35:16 +0800

    常用的功能函数
    1、print_msg : 打印信息函数；
    打印信息并带有分隔符以方便查看，支持打印多个信息

---
+++ func.py
@@ -0,0 +1,21 @@
+
+def print_msg(*msg,sign='=',num=50):
+    msg_num=len(msg)
+    sign=sign*num
+    s='~'
+    s_num=int(num*1.2)
+    s=s*s_num
+    print('\n{}'.format(sign))
+    if msg_num==1:
+        for i in msg:
+            one_msg='{}'.format(i)
+            print(one_msg)
+    elif msg_num>1:
+        for i in msg:
+            one_msg='{}'.format(i)
+            print(one_msg)
+            if i!=msg[-1]:
+                print(s)
+
+    print('{}'.format(sign))
+# print_msg('da的',1,[1,2],(21,21),{1:'21'},sign='=')
